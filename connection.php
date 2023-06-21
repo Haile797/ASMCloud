d0bf489709f57abfea40b62da6660cf1fa709c0e
Haile797 <Hailvgcc200225@fpt.edu.vn>
2023-06-21 13:39:25 +0700

    update5

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     // $conn = mysqli_connect('localhost','root','','asmcloud') 
     // or die("Cannot connect database".mysqli_connect_error());
 
-    $conn = mysqli_connect("s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","q438pn5sh6k1ffgq","amp1d4fangrnb25n","xc1206mcl8v7lh2v") 
+    $conn = mysqli_connect("s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","fwghmmsbjw0gu1pw","j0n3jnwfd58o894m","a1c2nuj5oycrm237") 
     or die("Cannot connect database".mysqli_connect_error());
 
     mysqli_query($conn, 'SET NAMES "utf8"')
